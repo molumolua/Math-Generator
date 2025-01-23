fe14508a99f26201eb06ce4790dfb9554392c9e7
molumolua <1005389104@qq.com>
2025-01-23 20:45:56 +0800

    add filter_problems

--- util/config.py
+++ util/config.py
@@ -1,6 +1,6 @@
 # config.py
 
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 TRAINING_DATA_PATH = './MATH/train'  # 数据库的根路径
 OUTPUT_PATH = './MATH/answers'       # 存储答案的路径
 TRAINING_DATA_PATH_AIME = './AIME_new'  # 数据库的根路径
