31d7e53e30d7cec11b66e3ac0cb5a3f049bde1d7
molumolua <1005389104@qq.com>
2025-01-25 20:50:46 +0800

    get data

--- generate_data.py
+++ generate_data.py
@@ -1,22 +1,23 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import time
 import json
 from util import util, set_logger
 import csv
 import sys
 import os
-import json
 import random
-import sys
+import math
 from tqdm import tqdm
 from data.data_loader import load_problems
-from prompt.prompt_design import createComplexQuestionPrompt
-import math
+from prompt.prompt_design import createComplexQuestionProcessPrompt
+from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-def main(batch_size=64,
+
+def main(stop_words = ["</s>", "<|im_end|>", "<|endoftext|>","\n**Complexification Process**"],
+         max_tokens=4096,
          device="cuda",
-         output_path="./outputs/complex_question.json",
-         model_name_or_path="/home/bingxing2/home/scx8q73/jobs/LLaMA-Factory-main/models/qwen2.5-Math-1.5b/full/sft"):
+         output_path="./outputs/complex_question_process_1.5b_math.json",
+         model_name_or_path="/home/bingxing2/home/scx8q73/jobs/LLaMA-Factory-main/models/qwen2.5-Math-1.5b-process/full/sft"):
     logger = set_logger.setup_logger()
     logger.info("Starting the process...")
 
@@ -25,78 +26,70 @@ def main(batch_size=64,
     problems = load_problems(iteration=None, min_level=1, max_level=5)
     logger.info(f"Loaded {len(problems)} problems.")
 
-    # Load model and tokenizer
+    # Load vLLM model
     logger.info(f"Loading model from {model_name_or_path}...")
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name_or_path,
-        torch_dtype="auto",
-        device_map="auto"
+    model = LLM(model_name_or_path, device=device)
+    tokenizer = AutoTokenizer.from_pretrained(
+                model_name_or_path, trust_remote_code=True
     )
-    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, padding_side="left")
-    logger.info("Model and tokenizer loaded.")
+    logger.info("Model loaded successfully.")
 
+    # Apply_chat_template
     output_list = []
-    total_batch = math.ceil(len(problems) / batch_size)
-    logger.info(f"Total batches: {total_batch}")
-
-    for batch_idx in range(total_batch):
-        logger.info(f"Processing batch {batch_idx + 1}/{total_batch}...")
-
-        # Batch process problems into message
-        messages_batch = []
-        start_idx = batch_idx * batch_size
-        end_idx = min((batch_idx + 1) * batch_size, len(problems))
-        now_problems = problems[start_idx:end_idx]
-
-        for problem in now_problems:
-            message = [
-                {
-                    "role": "system",
-                    "content": "You are a helpful assistant."
-                },
-                {
-                    "role": "user",
-                    "content": createComplexQuestionPrompt(problem['problem'], problem['solution'])
-                }
-            ]
-            messages_batch.append(message)
-        # 使用apply_chat_template批量处理
-        texts = [tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True) for messages in messages_batch]
-        # 将处理后的文本转化为模型输入
-        model_inputs = tokenizer(texts, return_tensors="pt",padding=True,truncation=True).to(device)
-        
-        # 生成文本
-        generated_ids = model.generate(
-            model_inputs.input_ids,
-            max_new_tokens=2048
+    now_problems = problems
+    input_texts = [
+        tokenizer.apply_chat_template(
+            [{"role": "user", "content": createComplexQuestionProcessPrompt(problem['problem'], problem['solution'])}],
+            tokenize=False,
+            add_generation_prompt=True,
         )
-        generated_ids_trimmed = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
+        for problem in now_problems
+    ]
+    input_texts= [
+        "<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n"+ \
+        createComplexQuestionProcessPrompt(problem['problem'], problem['solution'])+ \
+        "<|im_end|>\n<|im_start|>assistant\n"
+        for problem in now_problems
+    ]
+    logger.info(input_texts[0])
         
-        # 解码生成的文本
-        generated_responses = tokenizer.batch_decode(generated_ids_trimmed, skip_special_tokens=True)
+    # Define sampling parameters
+    sampling_params = SamplingParams(
+        max_tokens=max_tokens,
+        temperature=0.7,
+        stop=stop_words,
+        n=1
+    )
+
+    # Generate responses using vLLM
+    logger.info("Generating responses...")
+    generated_responses = model.generate(input_texts, sampling_params=sampling_params)
+    generated_responses = [generated_response.outputs[0].text for generated_response in generated_responses]
+    # Process the generated responses
+    for problem, response in zip(now_problems, generated_responses):
+        process, complex_problem, complex_solution = util.parse_answer(response, 
+                                                                        ["Complexification Process", 
+                                                                        "Complexified Problem", 
+                                                                        "Complexified Solution"], 
+                                                                        logger=logger)
+        output_object = {
+            "original_problem": problem['problem'],
+            "original_solution": problem['solution'],
+            "complex_problem": complex_problem,
+            "complex_solution": complex_solution,
+            "response": response,
+            "Complexification Process": process
+        }
+        output_list.append(output_object)
 
 
-        # 输出生成的结果
-        for problem, response in zip(now_problems, generated_responses):
-            complex_problem, complex_solution = util.parse_answer(response, ["Complexified Problem", "Complexified Solution"], logger=logger)
-            output_object = {
-                "original_problem": problem['problem'],
-                "original_solution": problem['solution'],
-                "complex_problem": complex_problem,
-                "complex_solution": complex_solution,
-                "response":response
-            }
-            output_list.append(output_object)
-
-        logger.info(f"Batch {batch_idx + 1}/{total_batch} processed.")
-        # Save the output to a JSON file
-        logger.info(f"Saving output to {output_path}...")
-        with open(output_path, 'w', encoding='utf-8') as output_json:
-            json.dump(output_list, output_json, ensure_ascii=False, indent=4)
+    # Save the output to a JSON file
+    logger.info(f"Saving output to {output_path}...")
+    with open(output_path, 'w', encoding='utf-8') as output_json:
+        json.dump(output_list, output_json, ensure_ascii=False, indent=4)
 
     logger.info("Process completed.")
+
 
 if __name__ == "__main__":
     main()
