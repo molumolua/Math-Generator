56947927583b9d047b952c1cbd3f5aed5365a774
molumolua <1005389104@qq.com>
2025-01-31 14:07:46 +0800

    add new test on 23

--- process_train_data.py
+++ process_train_data.py
@@ -2,44 +2,44 @@ import os
 import json
 from prompt.prompt_design import createComplexQuestionProcessPrompt
 problems = []
-now_path="./outputs/filter_complex_question_process_1.5b_math.json"
-result_path="./outputs/train_data_filter_complex_question_process_1.5b_math.json"
+now_path="./outputs_23/outputs_process_23.json"
+result_path="./outputs_23/train_data_process_23.json"
 file_path = os.path.join(now_path)
 with open(file_path, 'r', encoding='utf-8') as f:
     data_list = json.load(f)
     for data in data_list:   
-        # problem={
-        #     'messages':[
-        #         {
-        #             "role": "system",
-        #             "content": "You are a helpful assistant."
-        #         },
-        #         {
-        #             "role": "user",
-        #             "content": createComplexQuestionProcessPrompt(data['problem'],data['solution'])
-        #         },
-        #         {
-        #             "role": "assistant",
-        #             "content": "**Complexification Process**:\n{}\n**Complexified Problem**:\n{}\n\n**Complexified Solution**:\n{}\n".format(data['complexify_process'],data['original_problem'],data['original_solution'])
-        #         }
-        #     ]
-        # }
         problem={
             'messages':[
                 {
                     "role": "system",
-                    "content": "\nPlease reason step by step, and put your final answer within \\boxed{{}}."
+                    "content": "You are a helpful assistant."
                 },
                 {
                     "role": "user",
-                    "content": data['problem']
+                    "content": createComplexQuestionProcessPrompt(data['problem'],data['solution'])
                 },
                 {
                     "role": "assistant",
-                    "content": data['solution']
+                    "content": "**Complexification Process**:\n{}\n**Complexified Problem**:\n{}\n\n**Complexified Solution**:\n{}\n".format(data['complexify_process'],data['original_problem'],data['original_solution'])
                 }
             ]
         }
+        # problem={
+        #     'messages':[
+        #         {
+        #             "role": "system",
+        #             "content": "\nPlease reason step by step, and put your final answer within \\boxed{{}}."
+        #         },
+        #         {
+        #             "role": "user",
+        #             "content": data['problem']
+        #         },
+        #         {
+        #             "role": "assistant",
+        #             "content": data['solution']
+        #         }
+        #     ]
+        # }
         problems.append(problem)
 with open(result_path, 'w', encoding='utf-8') as output_json:
     json.dump(problems, output_json, ensure_ascii=False, indent=4)
